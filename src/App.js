6595a47d19e443dad8b67c0197d730603c1b1a70
muqadasurooj/17 <muqadasurooj123@gmail.com>
2022-06-14 00:50:06 +0500

    created a hero section in glassmorphism

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+import React from "react";
+// Components
+function App() {
+  return(
+    <>
+    <main className="container">
+      <div className="card-wrapper">
+        <div className="contents">
+          <h3>Muqadas</h3>
+          <p>Frontend developer</p>
+          <a href="">Readmore</a>
+        </div>
+      </div>
+    </main>
+  
+    </>
+  )
+}
+
+export default App;
